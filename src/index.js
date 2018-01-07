5f8868b6933a4c8b6186a604ebc626590c616448
alexbol99 <alexbol99@gmail.com>
2018-01-07 22:52:38 +0200

    Remove reading manifest file

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import '../public/styles/index.css';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { reducer } from './reducer';
 
-import about from './middleware/about';
+// import about from './middleware/about';
 import log from './middleware/log';
 import readFiles from './middleware/readFiles';
 import pasteData from './middleware/pasteData';
@@ -20,7 +20,7 @@ import matrix_test from './middleware/matrix-test';
 // import webgl_test from './middleware/webgl-test';
 
 const store = createStore(reducer, compose(applyMiddleware(
-    about,
+    /*about,*/
     log,
     readFiles,
     pasteData,
